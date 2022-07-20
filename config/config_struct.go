0539f26c0dd687e5f4e821b8f53399da9c62fa7e
mohammaderm <taherm76@yahoo.com>
2022-07-20 12:03:29 +0430

    project configs

---
+++ config/config_struct.go
@@ -0,0 +1,33 @@
+package config
+
+type (
+	Config struct {
+		Database Database `yaml:"database"`
+		Logger   Logger   `yaml:"logger"`
+		Server   Server   `yaml:"server"`
+	}
+	Database struct {
+		Postgresql Postgresql `yaml:"postgresql"`
+	}
+	Server struct {
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+	}
+	Postgresql struct {
+		Database  string `yaml:"database"`
+		Username  string `yaml:"username"`
+		Password  string `yaml:"password"`
+		Host      string `yaml:"host"`
+		Port      string `yaml:"port"`
+		Adabter   string `yaml:"adabter"`
+		Time_zone string `yaml:"time_zone"`
+		Charset   string `yaml:"charset"`
+	}
+	Logger struct {
+		Max_Age          string `yaml:"max_age"`
+		Max_Size         string `yaml:"max_size"`
+		Filename_Pattern string `yaml:"filename_pattern"`
+		Rotation_Time    string `yaml:"rotation_time"`
+		Internal_Path    string `yaml:"internal_path"`
+	}
+)
